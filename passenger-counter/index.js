51ccff0b15eedc0945d073647d8fdf7929911656
anjankarmakar10 <anjankarmakar15@gmail.com>
2023-01-25 19:58:02 +0600

    increment and decrement button add

--- passenger-counter/index.js
+++ passenger-counter/index.js
@@ -1,6 +1,16 @@
-
-
-function increment(){
-
+let count = 0;
+function increment() {
+  count++;
+  document.getElementById("count-el").innerText = count;
 }
 
+function decrement() {
+  if (count) {
+    count--;
+    document.getElementById("count-el").innerText = count;
+  }
+}
+
+document.getElementById("increment-btn").addEventListener("click", increment);
+
+document.getElementById("decrement-btn").addEventListener("click", decrement);
